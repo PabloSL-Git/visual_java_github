187418d0229f3da9245a2957c34f65496448f9cd
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-03 17:16:04 +0100

    completado 3/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/BolasColores.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/BolasColores.java
@@ -16,8 +16,8 @@ public class BolasColores {
         boolean salir = false;
         Random random = new Random();
 
-        final double BOLA_ROJA = 0.15;
-        final double BOLA_NARANJA = 0.25;
+        final double BOLA_ROJA = 0.85;
+        final double BOLA_NARANJA = 0.75;
         final double BOLA_AZUL = 0.50;
 
         do {
@@ -42,33 +42,43 @@ public class BolasColores {
                     } else if (precioCompra <= 0) {
                         JOptionPane.showMessageDialog(null, "Numero deve ser mayor que 0");
                     }
-                } catch () {
+                } catch (NumberFormatException nfe) {
+                    JOptionPane.showMessageDialog(null, "Introduce solo numeros");
 
                 }
 
             }
 
             if (colorBola == 1) {
-                String ColorBolatxt = "rojo";
+                JOptionPane.showMessageDialog(null, "Te toco bola roja a 15%");
             } else if (colorBola == 2) {
-                String ColorBolatxt = "naranja";
+                JOptionPane.showMessageDialog(null, "Te toco bola naranja a 25%");
             } else if (colorBola == 3) {
-                String ColorBolatxt = "azul";
+                JOptionPane.showMessageDialog(null, "Te toco bola azul a 50%");
             } else if (colorBola == 4) {
-                String ColorBolatxt = "negro";
+                JOptionPane.showMessageDialog(null, "Te toco bola negra sin descuento");
             }
 
             switch (colorBola) {
                 case 1 -> { // rojo
-
+                    double resultado = precioCompra * BOLA_ROJA;
+                    JOptionPane.showMessageDialog(null, "Su coste con descuento es: " + resultado);
                 }
                 case 2 -> { // naranja
+                    double resultado = precioCompra * BOLA_NARANJA;
+
+                    JOptionPane.showMessageDialog(null, "Su coste con descuento es: " + resultado);
 
                 }
                 case 3 -> { // azul
+                    double resultado = precioCompra * BOLA_AZUL;
+
+                    JOptionPane.showMessageDialog(null, "Su coste con descuento es: " + resultado);
 
                 }
                 case 4 -> { // negro
+
+                    JOptionPane.showMessageDialog(null, "La bola negra no ofrece descuento quedandote con:" + precioCompra);
 
                 }
 
