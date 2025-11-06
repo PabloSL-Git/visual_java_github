5717f95a5f37e3aa6d674e7387ac7ea7fb56b102
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-06 12:46:31 +0100

    EjercicioArray 6/11/25

---
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray.java
@@ -0,0 +1,27 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class PracticaArray {
+
+    private static final Scanner tec = new Scanner(System.in);
+    public static void main(String[] args) {
+        /*
+         * Sumar los elementos de un array y mostrar resultado
+         * 
+         * Media aritmética de los números de un array
+         * 
+         * Contar números de veces que aparece un n,umero de un array
+         */
+
+        int[] array1 = { 15, 6, 3, 7, 11, 8 };
+
+
+
+
+        tec.close();
+
+
+    }
+
+}
