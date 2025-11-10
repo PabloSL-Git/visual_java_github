76b564943b855baa739a1aacd2385da26fc2fd16
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-10 08:29:46 +0100

    termina array2 10/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray2.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray2.java
@@ -1,10 +1,13 @@
 package Ejercicios;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class PracticaArray2 {
 
     private static final Scanner tec = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         /*
@@ -27,15 +30,88 @@ public class PracticaArray2 {
          * 
          */
 
+        // 1
 
+        int[] numeros = { 15, 6, 3, 7, 15, 8 };
+        double[] decimales = { 2.5, 4.0, 6.5 };
+        String[] palabras1 = { "hola", "mundo", "java" };
+        String[] palabras2 = { "hola", "mundo", "java" };
 
+        // 1. Valor máximo
+        int max = valorMaximo(numeros);
+        System.out.println("El valor máximo es: " + max);
 
+        // 2. Valor mínimo
+        int min = valorMinimo(numeros);
+        System.out.println("El valor mínimo es: " + min);
 
+        // 3. Multiplicar elementos de un array de double por un número
+        System.out.println("Array original: " + Arrays.toString(decimales));
+        multiplicaArray(decimales, 2.0);
+        System.out.println("Array multiplicado: " + Arrays.toString(decimales));
 
+        // 4. Generar array de letras aleatorias
+        char[] letras = generaArrayLetras(10);
+        System.out.println("Array de letras generado: " + Arrays.toString(letras));
 
+        // 5. Comparar dos arrays de String
+        boolean iguales = arraysIguales(palabras1, palabras2);
+        System.out.println("¿Los arrays son iguales? " + iguales);
 
         tec.close();
-        
+    }
+
+    // 1. Valor máximo
+    public static int valorMaximo(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    // 2. Valor mínimo
+    public static int valorMinimo(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    // 3. Multiplicar todos los elementos de un array de double
+    public static void multiplicaArray(double[] array, double numero) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= numero;
+        }
+    }
+
+    // 4. Generar array de letras aleatorias entre 'a' y 'z'
+    public static char[] generaArrayLetras(int tamaño) {
+        Random random = new Random();
+        char[] letras = new char[tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            letras[i] = (char) ('a' + random.nextInt(26));
+        }
+        return letras;
+    }
+
+    // 5. Comparar si dos arrays de String son iguales
+    public static boolean arraysIguales(String[] array1, String[] array2) {
+        if (array1.length != array2.length) {
+            return false;
+        }
+        for (int i = 0; i < array1.length; i++) {
+            if (!array1[i].equals(array2[i])) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
 }
