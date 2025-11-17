fde28f2d6ad3524f1b98c732844971f1ab5b9fbb
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-17 08:30:21 +0100

    practica array 17/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray3.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray3.java
@@ -7,36 +7,49 @@ public class PracticaArray3 {
     public static void main(String[] args) {
 
         // 7. array 100 numeros aleatorios entre 1 y 30
-        // * 8. Funcion que recibe aray del 7. que nos devolvera el array sin duplicados
+        // 8. Funcion que devuelve ese array sin duplicados
 
         int[] numerosCambiados = arrayNoRepetido();
 
         for (int i = 0; i < numerosCambiados.length; i++) {
-
             System.out.println("EL numero cambiado de " + (1 + i) + " es " + numerosCambiados[i]);
-
         }
     }
 
     public static int[] arrayNoRepetido() {
 
-        int[] numeros = new int[100];
-        Random randomNumeros = new Random();
-        int[] numerosCambiados = new int[100];
+    int[] numeros = new int[100];
+    Random random = new Random();
 
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = randomNumeros.nextInt(1, 31);
-        }  
-
-        for (int j = 0; j < numerosCambiados.length; j++) {
-            numerosCambiados[j] = numeros[j];
-        }
-        // Cuando j es por ejemplo 4, have que numeroCambiado sea 4 y numero sea 4
-
-        
-
-
-        return numerosCambiados;
+    // Generar array original
+    for (int i = 0; i < numeros.length; i++) {
+        numeros[i] = random.nextInt(1, 31);
     }
+
+    // Crear array para guardar únicos
+    int[] unicos = new int[100];
+    int contador = 0;
+
+    for (int i = 0; i < numeros.length; i++) {
+
+        boolean repetido = false;
+
+        // revisar si ya está en unicos
+        for (int j = 0; j < contador; j++) {
+            if (numeros[i] == unicos[j]) {
+                repetido = true;
+                break;
+            }
+        }
+
+        if (!repetido) {
+            unicos[contador] = numeros[i];
+            contador++;
+        }
+    }
+
+    // devolver solo la parte usada
+    return Arrays.copyOf(unicos, contador);
+}
 
 }
