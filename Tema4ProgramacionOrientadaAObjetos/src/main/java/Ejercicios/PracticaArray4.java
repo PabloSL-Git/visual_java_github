c285c0f8624b516fb73bd1eeef41c86a2a3c8d47
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-18 12:16:40 +0100

    array4 18/11/25

---
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray4.java
@@ -0,0 +1,83 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class PracticaArray4 {
+
+    private static final Scanner tec = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // pedir a usuario que meta en array 10 posiciones numeros del 1 al 5
+        // contar cuantos 1 hay, cuantos 2 hay , etc
+
+        int[] array1 = new int[10];
+        System.out.println("Dame 10 numeros del 1 al 5");
+        int buscador1 = 0;
+        int buscador2 = 0;
+        int buscador3 = 0;
+        int buscador4 = 0;
+        int buscador5 = 0;
+        boolean valido = false;
+
+        for (int i = 0; i < array1.length; i++) {
+
+            System.out.println("Dame el numero de la siguiente posicion " + i);
+            array1[i] = tec.nextInt();
+
+            while (!valido) { // Repite hasta que el usuario escriba algo correcto
+
+            if (array1[i] < 1 || array1[i] > 5 ) {
+                JOptionPane.showMessageDialog(null, "Deve se entre 1 y 5");
+
+            } else {
+                valido = true;
+            }
+
+        }
+
+        }
+
+         for (int i = 0; i < array1.length; i++) {
+
+            System.out.println("En array posicion " + i + " es " + array1[i]);
+
+        }
+
+
+        for (int i = 0; i < array1.length - 1; i++) {
+            
+
+                if (array1[i] == 1) {
+                    buscador1++;
+                }
+                if (array1[i] == 2) {
+                    buscador2++;
+                }
+                if (array1[i] == 3) {
+                    buscador3++;
+                }
+                if (array1[i] == 4) {
+                    buscador4++;
+                }
+                if (array1[i] == 5) {
+                    buscador5++;
+                }
+                
+
+            
+        }
+
+        System.out.println("Numero de 1: " + buscador1);
+        System.out.println("Numero de 2: " + buscador2);
+        System.out.println("Numero de 3: " + buscador3);
+        System.out.println("Numero de 4: " + buscador4);
+        System.out.println("Numero de 5: " + buscador5);
+
+        
+
+        tec.close();
+
+    }
+
+}
--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/Primo10Veces.java
+++
@@ -1,9 +0,0 @@
-package Ejercicios;
-
-public class Primo10Veces {
-    public static void main(String[] args) {
-        // llama 10 veces al metodo primo
-        // euclides
-    }
-
-}
