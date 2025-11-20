6e5df20b99fdc33a53e201724d0e1872fb845e1a
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-20 11:04:27 +0100

    array4 con funciones (very cool) 20/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray4.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/PracticaArray4.java
@@ -11,18 +11,36 @@ public class PracticaArray4 {
         // pedir a usuario que meta en array 10 posiciones numeros del 1 al 5
         // contar cuantos 1 hay, cuantos 2 hay , etc
 
+        int[] array1 = Primero();
+
+        for (int i = 0; i < array1.length; i++) {
+
+            System.out.println("En array posicion " + i + " es " + array1[i]);
+
+        }
+
+        int[] array2 = Segundo(array1); // esta funcion sera para poner cada contado en un array para devolver
+
+        System.out.println("Numero de 1: " + array2[0]);
+        System.out.println("Numero de 2: " + array2[1]);
+        System.out.println("Numero de 3: " + array2[2]);
+        System.out.println("Numero de 4: " + array2[3]);
+        System.out.println("Numero de 5: " + array2[4]);
+
+        tec.close();
+
+        // problemas no deberia coger numeros superior o inferior a 1 y 5
+
+    }
+
+    public static int[] Primero() { // funcion, se le pone int[] para devolver array
+
         int[] array1 = new int[10];
         System.out.println("Dame 10 numeros del 1 al 5");
-        int buscador1 = 0;
-        int buscador2 = 0;
-        int buscador3 = 0;
-        int buscador4 = 0;
-        int buscador5 = 0;
-        
+
         for (int i = 0; i < array1.length; i++) {
 
             boolean valido = false;
-
 
             System.out.println("Dame el numero de la siguiente posicion " + (i));
             array1[i] = tec.nextInt();
@@ -37,16 +55,21 @@ public class PracticaArray4 {
                 } else {
                     valido = true;
                 }
-
             }
-
         }
+        return array1;
 
-        for (int i = 0; i < array1.length; i++) {
+    }
 
-            System.out.println("En array posicion " + i + " es " + array1[i]);
+    public static int[] Segundo(int[] array1) {
 
-        }
+        int[] array2 = new int[5];
+
+        int buscador1 = 0;
+        int buscador2 = 0;
+        int buscador3 = 0;
+        int buscador4 = 0;
+        int buscador5 = 0;
 
         for (int i = 0; i < array1.length; i++) {
 
@@ -68,15 +91,13 @@ public class PracticaArray4 {
 
         }
 
-        System.out.println("Numero de 1: " + buscador1);
-        System.out.println("Numero de 2: " + buscador2);
-        System.out.println("Numero de 3: " + buscador3);
-        System.out.println("Numero de 4: " + buscador4);
-        System.out.println("Numero de 5: " + buscador5);
+        array2[0] = buscador1;
+        array2[1] = buscador2;
+        array2[2] = buscador3;
+        array2[3] = buscador4;
+        array2[4] = buscador5;
 
-        tec.close();
-
-        // problemas no deberia coger numeros superior o inferior a 1 y 5
+        return array2;
 
     }
 
