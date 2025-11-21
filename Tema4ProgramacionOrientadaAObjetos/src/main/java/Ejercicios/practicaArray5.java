619ae6f947417a072fd90f6d975883d93f6d0520
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-21 08:46:40 +0100

    continuo array5 21/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/practicaArray5.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/Ejercicios/practicaArray5.java
@@ -1,5 +1,8 @@
 package Ejercicios;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class practicaArray5 {
@@ -8,23 +11,40 @@ public class practicaArray5 {
     public static void main(String[] args) {
         // funcion de copiar array que se le de y darle la vuelta
 
-        int[] array1 = LectorArray();
+        int[] array1 = GenerarArray();
 
-        
+        int[] copia = new int[array1.length];
+
+        int[] copiaInvertida = InvertirArray(copia);
+
+        System.out.println("Array dado la vuelta es: " + Arrays.toString(copiaInvertida));
+
     }
 
-    public static int[] LectorArray() { // funcion, se le pone int[] para devolver array
+    public static int[] GenerarArray() { // funcion, se le pone int[] para devolver array
 
-        int[] array1 = new int[10];
+        int[] array1 = new int[5];
 
         for (int i = 0; i < array1.length; i++) {
 
+            Random random = new Random();
 
-            System.out.println("Dame el numero de la siguiente posicion " + (i));
-            array1[i] = tec.nextInt();
+            array1[i] = random.nextInt(10);
 
         }
+
+        System.out.println("Array 1 es: " + Arrays.toString(array1));
         return array1;
+    }
+
+    public static int[] InvertirArray(int[] copia) {
+
+        int[] copiaInvertida = new int[copia.length];
+
+        // parte que invierte
+
+        return copiaInvertida;
+
     }
 
 }
