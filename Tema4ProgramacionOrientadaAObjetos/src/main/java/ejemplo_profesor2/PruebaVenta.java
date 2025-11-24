f31347f80b6ea5e8a03cdd9acec38b405931c0ae
PabloSL-Git <santamarialoredo@gmail.com>
2025-11-24 12:47:01 +0100

    metodos 24/11/25

--- Tema4ProgramacionOrientadaAObjetos/src/main/java/ejemplo_profesor2/PruebaVenta.java
+++ Tema4ProgramacionOrientadaAObjetos/src/main/java/ejemplo_profesor2/PruebaVenta.java
@@ -5,18 +5,26 @@ public class PruebaVenta {
     public static void main(String[] args) {
 
         // Instanciación de un objeto de tipo Venta
-        // Para instanciar necesito el constructor y usar el 
+        // Para instanciar necesito el constructor y usar el
         // operador new --> reservar memoria
-        // 
-        Venta v1 = new Venta("AgriculturaEstepona",
-         "Fina", "Picual",100);
 
-        v1.mostrarValores();
+        // Venta v1 = new Venta("AgriculturaEstepona",
+        //         "Fina", "Picual", 100, 200.34);
 
-        Venta v2 = new Venta("Casares","Gruesa",
-        "Hojiblanca" , 10);
-        v2.mostrarValores();
-        
+        Venta v2 = new Venta("Casares", "Gruesa",
+                "Hojiblanca", 10, 200.35);
+
+        System.out.println(v2.toString());
+        System.out.println(v2.getNombreProductor());
+        // v2.tamanioAceituna = "El que yo quiera";
+        System.out.println(v2.toString());
+
+        v2.setNombreProductor("Jaén");
+        System.out.println("-----------------");
+        System.out.println(v2);
+
+        v2.setTamanioAceituna("Fina");
+        System.out.println(v2);
     }
 
 }
